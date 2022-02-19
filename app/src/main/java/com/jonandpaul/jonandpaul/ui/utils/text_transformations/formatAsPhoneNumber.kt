e7f82a924c6d17973ad05e085209058a94ddc603
Daniel Atitienei <daniatitienei@gmail.com>
2022-02-19 21:55:44 +0200

    Open tracking url done

--- app/src/main/java/com/jonandpaul/jonandpaul/ui/utils/text_transformations/formatAsPhoneNumber.kt
+++ app/src/main/java/com/jonandpaul/jonandpaul/ui/utils/text_transformations/formatAsPhoneNumber.kt
@@ -1,5 +1,4 @@
 package com.jonandpaul.jonandpaul.ui.utils.text_transformations
 
-fun String.formatAsPhoneNumber(): String {
-    return this.substring(0, 3) + " " + substring(3, 6) + " " + substring(6, 9)
-}
+fun String.formatAsPhoneNumber(): String =
+    this.substring(0, 3) + " " + substring(3, 6) + " " + substring(6, 9)
